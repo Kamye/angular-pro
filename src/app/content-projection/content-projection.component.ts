a61d80d4d4ba6ab927772e2f76d18e5d996db39d
Kamye <andrej15ganzha@gmail.com>
2022-06-27 15:37:29 +0300

    Using ElementRef and nativeElement

--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -5,13 +5,6 @@ import {User} from "./auth-form/auth-form.interface";
     selector: 'content-projection',
     template: `
         <div style="display: flex">
-            <auth-form (submitted)="createUser($event)">
-                <h3>Create account</h3>
-                <button type="submit">
-                    Join Us
-                </button>
-            </auth-form>
-
             <auth-form
                 (submitted)="loginUser($event)">
                 <h3>Login</h3>
