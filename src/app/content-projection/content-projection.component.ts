c6b3fc069a35f3e6b39781d1d3908200f9c65f99
Kamye <andrej15ganzha@gmail.com>
2022-07-14 22:00:29 +0300

    Dynamic template rendering with ViewContainerRef

--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -2,34 +2,26 @@ import {
     AfterViewInit,
     ChangeDetectorRef,
     Component,
-    ComponentFactoryResolver, ComponentRef,
+    ComponentFactoryResolver, TemplateRef,
     ViewChild,
     ViewContainerRef
 } from '@angular/core';
-
-import {AuthFormComponent} from './auth-form/auth-form.component';
-
-import {User} from './auth-form/auth-form.interface';
 
 @Component({
     selector: 'content-projection',
     template: `
         <div>
-            <button (click)="destroyComponent()">
-                Destroy
-            </button>
-            <button (click)="moveComponent()">
-                Move
-            </button>
             <div #entry></div>
+            <ng-template #tmpl>
+                Andriy Hanzha : Dnipro, UA
+            </ng-template>
         </div>
     `
 })
 export class ContentProjectionComponent implements AfterViewInit{
 
-    private component!: ComponentRef<AuthFormComponent>;
-
     @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
+    @ViewChild('tmpl',) tmpl!: TemplateRef<any>
 
     constructor(
         private resolver:  ComponentFactoryResolver,
@@ -38,24 +30,8 @@ export class ContentProjectionComponent implements AfterViewInit{
     }
 
     ngAfterViewInit() {
-        const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-        this.entry.createComponent(authFormFactory);
-        this.component = this.entry.createComponent(authFormFactory, 0);
-        this.component.instance.title = 'Create account';
-        this.component.instance.submitted.subscribe(this.loginUser);
-
+        this.entry.createEmbeddedView(this.tmpl);
         this.cdr.detectChanges();
     }
 
-    public destroyComponent(): void {
-        this.component.destroy();
-    }
-
-    public moveComponent(): void {
-        this.entry.move(this.component.hostView, 1);
-    }
-
-    public loginUser(user: User) {
-        console.log('Login user', user);
-    }
 }
