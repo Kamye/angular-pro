4ce5564cff6d0ebbe69698ffb90741abb350b620
Kamye <andrej15ganzha@gmail.com>
2022-07-14 21:07:20 +0300

    Dynamic components with ComponentFactoryResolver

--- src/app/content-projection/auth-form/auth-form.component.ts
+++ src/app/content-projection/auth-form/auth-form.component.ts
@@ -2,13 +2,7 @@ import {
     Component,
     EventEmitter,
     Output,
-    ContentChildren,
-    QueryList,
-    AfterContentInit, ViewChildren, AfterViewInit, ChangeDetectorRef, ViewChild, ElementRef, Renderer2,
 } from '@angular/core';
-
-import {AuthRememberComponent} from './auth-remember.component';
-import {AuthMessageComponent} from './auth-message.component';
 
 import {User} from './auth-form.interface';
 
@@ -19,8 +13,8 @@ import {User} from './auth-form.interface';
     `],
     template: `
         <div>
-            <form (submit)="onSubmit(form.value)" #form="ngForm">
-                <ng-content select="h3"></ng-content>
+            <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
+                <h3>{{ title }}</h3>
                 <label>
                     Email address
                     <input type="email" name="email" ngModel #email>
@@ -29,56 +23,18 @@ import {User} from './auth-form.interface';
                     Password
                     <input type="password" name="password" ngModel>
                 </label>
-                <ng-content select="auth-remember"></ng-content>
-                <auth-message
-                    [style.display]="(showMessage ? 'inherit' : 'none')">
-                </auth-message>
-                <ng-content select="button"></ng-content>
+                <button type="submit">
+                    {{ title }}
+                </button>
             </form>
         </div>
-    `,
+    `
 })
-export class AuthFormComponent implements AfterContentInit, AfterViewInit {
-    showMessage: boolean = false;
+export class AuthFormComponent {
 
-    @ViewChild('email') email: ElementRef | undefined;
-
-    @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent> | undefined;
-
-    @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent> | undefined;
+    public title = 'Login'
 
     @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-
-    constructor(
-        private cdr: ChangeDetectorRef,
-        private renderer: Renderer2
-    ) {
-    }
-
-    ngAfterContentInit() {
-        if (this.remember) {
-            this.remember.forEach((item: AuthRememberComponent) => {
-                item.checked.subscribe((checked: boolean) => this.showMessage = checked);
-            });
-        }
-    }
-
-    ngAfterViewInit() {
-        this.renderer.setAttribute(this.email?.nativeElement, 'placeholder', 'Enter your email address');
-        this.renderer.addClass(this.email?.nativeElement, 'email');
-        this.renderer.selectRootElement(this.email?.nativeElement).focus();
-
-        // this.email?.nativeElement.setAttribute('placeholder', 'Enter your email address');
-        // this.email?.nativeElement.classList.add('email');
-        // this.email?.nativeElement.focus();
-
-        if (this.message) {
-            this.message.forEach((message: AuthMessageComponent) => {
-                message.days = 30;
-            });
-            this.cdr.detectChanges();
-        }
-    }
 
     public onSubmit(value: User) {
         this.submitted.emit(value);
--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -1,35 +1,42 @@
-import {Component} from '@angular/core';
-import {User} from "./auth-form/auth-form.interface";
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ComponentFactoryResolver,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
+
+import {AuthFormComponent} from './auth-form/auth-form.component';
+
+import {User} from './auth-form/auth-form.interface';
 
 @Component({
     selector: 'content-projection',
     template: `
         <div style="display: flex">
-            <auth-form
-                (submitted)="loginUser($event)">
-                <h3>Login</h3>
-                <auth-remember (checked)="rememberUser($event)"></auth-remember>
-                <button type="submit">
-                    Login
-                </button>
-            </auth-form>
+            <div #entry></div>
         </div>
     `
 })
-export class ContentProjectionComponent {
+export class ContentProjectionComponent implements AfterViewInit{
 
-    private rememberMe: boolean = false;
+    @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
 
+    constructor(
+        private resolver:  ComponentFactoryResolver,
+        private cdr: ChangeDetectorRef
+    ) {
+    }
 
-    public createUser(user: User) {
-        console.log('Create account', user, this.rememberMe);
+    ngAfterViewInit() {
+        const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+        this.entry.createComponent(authFormFactory);
+
+        this.cdr.detectChanges();
     }
 
     public loginUser(user: User) {
-        console.log('Login user', user, this.rememberMe);
-    }
-
-    public rememberUser(remember: boolean) {
-        this.rememberMe = remember;
+        console.log('Login user', user);
     }
 }
