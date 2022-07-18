7d3697ba8116fba55110e5baf73285088464387c
ahanzha <Hjlbkcz1994>
2022-07-18 15:02:25 +0300

    Dynamic template rendering with ngTemplateOutlet

--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -1,40 +1,18 @@
-import {
-    AfterViewInit,
-    ChangeDetectorRef,
-    Component,
-    ComponentFactoryResolver, TemplateRef,
-    ViewChild,
-    ViewContainerRef
-} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'content-projection',
     template: `
         <div>
-            <div #entry></div>
-            <ng-template #tmpl let-name let-location="location">
-                {{ name }} : {{ location }}
+            <ng-container
+                [ngTemplateOutlet]="tmpl">
+            </ng-container>
+            <ng-template #tmpl>
+                Andriy Hanzha: Dnipro, UA
             </ng-template>
         </div>
     `
 })
-export class ContentProjectionComponent implements AfterViewInit{
-
-    @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
-    @ViewChild('tmpl',) tmpl!: TemplateRef<any>
-
-    constructor(
-        private resolver:  ComponentFactoryResolver,
-        private cdr: ChangeDetectorRef
-    ) {
-    }
-
-    ngAfterViewInit() {
-        this.entry.createEmbeddedView(this.tmpl, {
-            $implicit: 'Hanzha Andriy',
-            location: 'UA, Dnipro'
-        });
-        this.cdr.detectChanges();
-    }
+export class ContentProjectionComponent {
 
 }
