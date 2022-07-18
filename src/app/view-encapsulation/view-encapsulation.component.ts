0c144e8986558af1a5f5a7c6c7ab934e6dd12040
Kamye <andrej15ganzha@gmail.com>
2022-07-18 22:13:14 +0300

    OnPush Change Detection and Immutability

--- src/app/view-encapsulation/view-encapsulation.component.ts
+++ src/app/view-encapsulation/view-encapsulation.component.ts
@@ -1,14 +1,40 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
     selector: 'view-encapsulation',
-    styles: [],
+    changeDetection: ChangeDetectionStrategy.Default,
     template: `
         <div>
-            <example-one></example-one>
-            <example-two></example-two>
-            <example-three></example-three>
+            <button (click)="addProp()">Add Property</button>
+            <button (click)="changeUser()">Change user object</button>
+            <button (click)="changeName()">Change name property</button>
+            <div class="users">
+                <example-one [user]="user"></example-one>
+                <example-two [user]="user"></example-two>
+            </div>
         </div>
     `
 })
-export class ViewEncapsulationComponent { }
+export class ViewEncapsulationComponent {
+    public user: any = {
+        name: 'Mark Hoppus',
+        age: 44,
+        location: 'California'
+    };
+
+    public addProp(): void {
+        this.user.email = 'some.email@mail.com';
+    }
+
+    public changeName(): void {
+        this.user.name = 'Travis Barker'
+    }
+
+    public changeUser(): void {
+        this.user = {
+            name: 'Tom Delonge',
+            age: 41,
+            location: 'Californis'
+        };
+    }
+}
